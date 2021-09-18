aab95387af1c4321419224fc08b34daef49ac9b6
Antti <anttituomola8@gmail.com>
2021-09-18 07:19:34 +0300

    started moduling the app

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ dayjs.extend(dayjs_plugin_dayOfYear)
 dayjs.extend(dayjs_plugin_duration)
 
 import Todo from "./todo.js";
+import deleteButtonHandler from "./deleteButtonHandler.js"
+import { render } from "./render.js";
+import { checkDate } from "./checkDate.js";
 
-let shifts = []
-let hours = 0
-let eveningHours = 0
+export let shifts = []
+export let hours = 0
+export let eveningHours = 0
 let shiftEveningHour = 0
 let shiftNightHour = 0
-let nightHours = 0
+export let nightHours = 0
 let shiftHour = 0
 
 //global variables
-let startDate = dayjs().hour(0).minute(0).format()
+export let startDate = dayjs().hour(0).minute(0).format()
 let startHour = dayjs(startDate).hour(12).minute(0)
 let endHour = dayjs(startDate).hour(19).minute(30)
 let button = document.getElementById("submitButton")
-let sortedShifts = shifts.sort((a, b) => b.startHour - a.startHour)
+export let sortedShifts = shifts.sort((a, b) => b.startHour - a.startHour)
 
 
 //input field placeholder values
@@ -25,8 +28,7 @@ document.getElementById("startDateEl").value = dayjs(startDate).format("YYYY-MM-
 document.getElementById("startHourEl").value = dayjs(startHour).format("HH:mm")
 document.getElementById("endHourEl").value = dayjs(endHour).format("HH:mm")
 
-
-function getValues() {
+export function getValues() {
     let startHourValue = document.getElementById("startHourEl").value
     startHourValue = startHourValue.split(":")
     let endHourValue = document.getElementById("endHourEl").value
@@ -39,24 +41,6 @@ function getValues() {
     }
     shiftHour = dayjs(endHour).diff(startHour, "h", true)
     document.getElementById("calculation").textContent = (Math.round(shiftHour * 100) / 100).toFixed(2)
-}
-
-//check if date already submitted
-function checkDate() {
-    getValues()
-    let thisDate = dayjs(startDate).format("DD.MM.YYYY")
-    let found = false
-    for (let i = 0; i < shifts.length; i++) {
-        if (shifts[i].startDate === thisDate) {
-            found = true
-            alert("This date already submitted!")
-            break
-        }
-    }
-    if (shifts.length === 0 || (!found)) {
-        updateHours()
-    }
-    console.log(shifts)
 }
 
 function countEveningHours() {
@@ -74,7 +58,7 @@ function countEveningHours() {
     }
 }
 
-function updateHours() {
+export function updateHours() {
     countEveningHours()
     //fix this: remove formatting from class element
     let todoEl = new Todo(startDate, startHour, endHour, shiftHour, shiftEveningHour, shiftNightHour)
@@ -97,39 +81,12 @@ function updateHours() {
     render()
 }
 
-function render() {
-    let shiftList = document.getElementById("shift-list")
-    let totalHoursEl = document.getElementById("calculationTotal")
-    shiftList.textContent = ""
-    for (let i = 0; i < sortedShifts.length; i++) {
-        shiftList.innerHTML +=
-            //TODO: instead of a float, render hours and minutes
-            `
-        <div class="aShitf">
-        <b>${sortedShifts[i].startDate.format("dddd DD.MM.YYYY")}:</b> ${(Math.round(sortedShifts[i].shiftHour * 100) / 100).toFixed(2)} h 
-        (${(Math.round(sortedShifts[i].shiftEveningHour * 100) / 100).toFixed(2)} evening hours)
-        <button id="${i}" class="deleteButton button">DELETE</button>
-        </div>`
-    }
-    //update date selector to the next day
-    startDate = dayjs(startDate).add(1, "d")
-    document.getElementById("startDateEl").value = dayjs(startDate).format("YYYY-MM-DD")
-
-    //render total hours
-    totalHoursEl.textContent = `${(Math.round(hours * 100) / 100).toFixed(2)} h (${(Math.round(eveningHours * 100) / 100).toFixed(2)} evening hours + ${nightHours} night hours)`
-}
-
 document.getElementById("shift-list").addEventListener("click", e => {
     while(e.target != null){
         if(e.target.classList.contains("deleteButton")) {deleteButtonHandler(e.target.id)}
         break
     }
 })
-
-function deleteButtonHandler(id) {  
-    shifts.splice(id, 1)
-        render()
-}
 
 //listen for changes
 document.getElementById("startDateEl").addEventListener("input", getValues)
