0b3ca5de13ec5d14bfe8a0c24a820d3b9e43d6b4
Antti <anttituomola8@gmail.com>
2021-09-19 19:16:14 +0300

    fixed evening counter

--- countEveningHours.js
+++ countEveningHours.js
@@ -6,17 +6,30 @@ import { startDate } from "./getValues.js"
 
 export function countEveningHours() {
     let eveningStarts = dayjs(startDate).hour(18)
-    if (dayjs(startHour).isAfter(eveningStarts)) {
+    let eveningEnds = dayjs(startHour).add(1, "day").hour(0).minute(0)
+    //if shift starts after 6pm and ends before midnight
+    if (dayjs(startHour).isAfter(eveningStarts) && dayjs(endHour).isBefore(eveningEnds)) {
         shiftEveningHour = endHour.diff(startHour, "hour", true)
-    } else if (dayjs(endHour).isAfter(eveningStarts)) {
+        //if shift starts before 6pm ends between 6pm and midnight
+    } else if (dayjs(startHour).isBefore(eveningStarts) && (dayjs(endHour).isAfter(eveningStarts) && dayjs(endHour).isBefore(eveningEnds))) {
         shiftEveningHour = endHour.diff(eveningStarts, "hour", true)
+        //if shift starts before 6pm and ends after midnight
+    } else if (dayjs(startHour).isBefore(eveningStarts) && (dayjs(endHour).isAfter(eveningEnds))) {
+        shiftEveningHour = eveningEnds.diff(eveningStarts, "hour", true)
+        //if shift starts after 6pm and ends after midnight
+    } else if (dayjs(startHour).isAfter(eveningStarts) && (dayjs(endHour).isAfter(eveningEnds))) {
+        shiftEveningHour = eveningEnds.diff(startHour, "hour", true)
     } else {
         shiftEveningHour = 0
     }
     //night hours
     if (dayjs(endHour).isAfter(dayjs(startDate), "day")) {
         let shiftNightStarts = dayjs(endHour).hour(0).minute(0)
+        let nightEnds = dayjs(endHour).hour(6).minute(0)
         let nightShiftEnds = dayjs(endHour)
+        if (dayjs(endHour).isAfter(nightEnds)) {
+            shiftNightHour = nightEnds.diff(shiftNightStarts, "hour", true)
+        }
         shiftNightHour = nightShiftEnds.diff(shiftNightStarts, "hour", true)
     }
 }
